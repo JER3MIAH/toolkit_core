0fefe88fa7495e4db309e8c9f211548e60919228
JER3MIAH <jeremiahbrownie@gmail.com>
2025-12-04 16:07:52 +0100

    fix: export theme properly

--- lib/src/themes/app_theme.dart
+++ lib/src/themes/app_theme.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_toolkit/flutter_toolkit.dart';
+import 'package:flutter_toolkit/src/themes/colors.dart';
 
 class AppTheme {
   AppTheme._();
