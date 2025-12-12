9b860c12df73bc7a9d20edc178c8854e35f2ef58
JER3MIAH <jeremiahbrownie@gmail.com>
2025-12-12 18:19:06 +0100

    feat: add bounce effect to [buildRoute] and make default

--- lib/src/utils/navigation/build_route.dart
+++ lib/src/utils/navigation/build_route.dart
@@ -1,27 +1,44 @@
 import 'package:flutter/material.dart';
 
-enum TransitionType { slide, fade, scale }
+enum TransitionType { slide, fade, scale, bounce }
 
 PageRouteBuilder buildRoute(
   Widget page,
   RouteSettings settings, {
-  TransitionType transition = TransitionType.slide,
+  TransitionType transition = TransitionType.bounce,
 }) {
   return PageRouteBuilder(
     settings: settings,
     pageBuilder: (_, _, _) => page,
     transitionsBuilder: (_, animation, secondaryAnimation, child) {
-      final curvedAnimation = CurvedAnimation(
-        parent: animation,
-        curve: Curves.easeInOut,
-      );
+      CurvedAnimation curvedAnimation;
 
       switch (transition) {
+        case TransitionType.bounce:
+          // Elastic curve for bounce effect
+          curvedAnimation = CurvedAnimation(
+            parent: animation,
+            curve: Curves.elasticOut,
+          );
+          return ScaleTransition(scale: curvedAnimation, child: child);
+
         case TransitionType.fade:
+          curvedAnimation = CurvedAnimation(
+            parent: animation,
+            curve: Curves.easeInOut,
+          );
           return FadeTransition(opacity: curvedAnimation, child: child);
         case TransitionType.scale:
+          curvedAnimation = CurvedAnimation(
+            parent: animation,
+            curve: Curves.easeInOut,
+          );
           return ScaleTransition(scale: curvedAnimation, child: child);
         default:
+          curvedAnimation = CurvedAnimation(
+            parent: animation,
+            curve: Curves.easeInOut,
+          );
           return SlideTransition(
             position: Tween(
               begin: const Offset(1, 0),
