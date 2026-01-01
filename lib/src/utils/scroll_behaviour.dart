c2e850e754529c620312884bf2b7b369f2ae111a
JER3MIAH <jeremiahbrownie@gmail.com>
2026-01-01 17:21:52 +0100

    chore: dart format project

--- lib/src/utils/scroll_behaviour.dart
+++ lib/src/utils/scroll_behaviour.dart
@@ -16,13 +16,19 @@ import 'package:flutter/material.dart';
 class NoThumbScrollBehavior extends ScrollBehavior {
   @override
   Widget buildOverscrollIndicator(
-      BuildContext context, Widget child, ScrollableDetails details) {
+    BuildContext context,
+    Widget child,
+    ScrollableDetails details,
+  ) {
     return child;
   }
 
   @override
   Widget buildScrollbar(
-      BuildContext context, Widget child, ScrollableDetails details) {
+    BuildContext context,
+    Widget child,
+    ScrollableDetails details,
+  ) {
     return child;
   }
 }
